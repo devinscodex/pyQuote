fc4adcc38cbfbc69a7e3bf0262526b4db818052d
Devin <ddwight@protonmail.com>
2022-12-28 08:23:46 -0600

    before leaving MEG 22-12-27

--- QuoteCompactor.py
+++ QuoteCompactor.py
@@ -6,31 +6,63 @@ import pandas as pd
 
 #pd.options.display.max_rows = None # toggle this line to display all rows or not
 
-# prerequisites (create path & load files to list for processing)
+# prerequisites & variables (validate/create path > add files to list for processing)
 folder_path = "c:/bin/quotes"
-quotes = []
+files = []
 
 if not os.path.exists(folder_path):
     print(folder_path + " does not exist...creating now...")
     os.makedirs(folder_path, exist_ok=True)
 
-# variables
-contents = os.listdir(folder_path)
 
-
-# loop through quote folder, add Excel files to list
+# loop folder, add Excel files to list, user selects file to read into Pandas
 for file in os.listdir(folder_path):
     if file.__contains__(".xls"):
-        quotes.append(file)
+        files.append(file)
 
-# for obj in contents:
-#     if os.path.isfile(obj):
-#         print("file found....")    
-#     if obj.__contains__(".xl"):
-#         quotes.append(obj) # add full path to quotes list
-#         #quotes.append(os.path.abspath(obj)) # add full path to quotes list <- this adds \\ in path...
+#print(quotes)
 
-print(quotes)
+print('Please select a file:')
+for i, file in enumerate(files):
+    print(f'{i + 1}: {file}')
+
+selection_made = False
+
+invalid_input = "Invalid selection, please select from the provide options:"
+
+while not selection_made:
+    selection = input('Enter a number: ')
+    try:
+        selection = int(selection)
+        # validate selection
+        if 1 <= selection <= len(files):
+            selected_file = os.path.abspath(files[selection - 1])
+            print(os.path.abspath(selected_file)
+            #df = pd.read_excel(selected_file)
+            selection_made = True
+        else:
+            print(invalid_input)
+    except ValueError:
+        print(invalid_input)
+    #except FileNotFoundError:
+    #selected_file = selected_file.replace('\\','/')
+
+#print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #df = pd.read_excel("c:/bin/quotes/sheet.xlsx", header=None)
 #print(df)
@@ -41,7 +73,6 @@ print(quotes)
     for row in sheet["A1:C72"]:
         if [x.value for x in row] == "x":
             print([x.value for x in row]) """
-
 
 
 """ if len(quotes) > 0:
