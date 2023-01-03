e90a925884d2b0103233798dc4e68627603999d6
Dev <ddwight@pm.me>
2023-01-03 06:32:22 -0600

    powershell script update

--- run.ps1
+++ run.ps1
@@ -4,21 +4,18 @@ if (!(get-command choco)) {
     Write-Output "Chocolatey is not installed, install now..."
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-else {
-    Write-Output "Choco ok"
-}
 
 if (!(get-command python)) {
     Write-Output "Python is not installed, installing now..."
     choco install python
 }
 else {
-    Write-Output "Python is installed, checking for necessary dependencies..."
     if (!(python -m pip freeze | select-string -Pattern "windows-curses")) {
         Write-Output "Windows-Curses package is not installed, installing now..."
         pip install windows-curses
     }
 }
+Write-Output "Status ok, running python now..."
 
-# all dependencies installed, run program
-python \\office\office\IT\library\software\programming-projects\python\rogueish.py
+python \\cerberus\fileserver\library\computers\software\programming\python\curses-tutorial-input+.py
+#python \\office\office\IT\library\software\programming-projects\python\rogueish.py
